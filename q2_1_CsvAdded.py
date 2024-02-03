ac5a92456e3ca7adee5209a6504392c292e2d4ea
Rahul Oberoi <118175583+oberoir080@users.noreply.github.com>
2024-02-03 17:13:04 +0530

    Top 5 bigram

--- q2_1_CsvAdded.py
+++ q2_1_CsvAdded.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pandas as pd
-import seaborn as sns
+# import seaborn as sns
 import random
+import csv
 
 class BigramLM:
     def __init__(self):
@@ -12,15 +13,19 @@ class BigramLM:
         self.totalcount=0
         self.bol="#"
         self.eol="$"
+        self.bigramCount = 0
 
     def add_data(self,corpus):
+        
         with open(corpus) as f:
             words = f.read().split()
         for i in range(len(words)-1):
             if words[i] not in  self.bigrams:
                 self.bigrams[words[i]]={}
+                self.bigramCount+=1
             if words[i+1] not in self.bigrams[words[i]]:
                 self.bigrams[words[i]][words[i+1]]=0
+                self.bigramCount+=1
             self.bigrams[words[i]][words[i+1]]+=1
             if words[i] not in self.unigrams:
                 self.unigrams[words[i]]=1
@@ -52,28 +57,41 @@ class BigramLM:
         df=pd.DataFrame(self.co_matrix,columns=self.uniquewords,index=self.uniquewords)
         # print(df)
         # print(sns.heatmap(df))
-        df.to_csv("CSVs/NoSmoothing.csv")
+        # df.to_csv("CSVs/NoSmoothing.csv")
         
     def Laplace_learn(self,k=1):
+        l=[]
         for i in range(len(self.uniquewords)):
             x=[]
+            
             
             for j in range(len(self.uniquewords)):
                 if self.uniquewords[i] in self.bigrams:
                     if self.uniquewords[j] in self.bigrams[self.uniquewords[i]]:
                         numerator = self.bigrams[self.uniquewords[i]][self.uniquewords[j]] + k #Reference for the formula Notes and https://www.youtube.com/watch?v=gCI-ZC7irbY
                         denominator = self.unigrams[self.uniquewords[i]] + (k*len(self.uniquewords))
-                        x.append(numerator/denominator)
+                        p = numerator/denominator
+                        l.append((p*(self.unigrams[self.uniquewords[i]]/self.totalcount),self.uniquewords[i],self.uniquewords[j]))
+                        x.append(p)
+                        # print("yes")
                     
                     else:
-                        x.append(k / (self.unigrams[self.uniquewords[i]] + (k * len(self.uniquewords))))
+                        p = k / (self.unigrams[self.uniquewords[i]] + (k * len(self.uniquewords)))
+                        x.append(p)
+                        l.append((p*(self.unigrams[self.uniquewords[i]]/self.totalcount),self.uniquewords[i],self.uniquewords[j]))
                 else:
-                    x.append(k / (k * len(self.uniquewords)))
+                    p = k / (k * len(self.uniquewords))
+                    x.append(p)
+                    l.append((p*(self.unigrams[self.uniquewords[i]]/self.totalcount),self.uniquewords[i],self.uniquewords[j]))
             self.co_matrix.append(x)
         df=pd.DataFrame(self.co_matrix,columns=self.uniquewords,index=self.uniquewords)
+        l.sort(reverse=True)
+        print(l[:5])
         # print(df)
         # print(sns.heatmap(df))
-        df.to_csv("CSVs/Laplace.csv")
+        # df.to_csv("CSVs/Laplace.csv")
+        
+
         
     def KneserNey_learn(self, d=0.75):
         for i in range(len(self.uniquewords)):
@@ -100,7 +118,7 @@ class BigramLM:
         df = pd.DataFrame(self.co_matrix, columns=self.uniquewords, index=self.uniquewords)
         # print(df)
         # sns.heatmap(df)
-        df.to_csv("CSVs/KneserNey.csv")
+        # df.to_csv("CSVs/KneserNey.csv")
 
 
     
@@ -134,10 +152,61 @@ class BigramLM:
             p=0
         # print(p)
         return p
+    
+    def find_top5_bigrams(self, output_file="top5prob.csv"):
+        l = []
+
+        with open(output_file, "w", newline='') as f:
+            csv_writer = csv.writer(f)
+            csv_writer.writerow(['Bigram', 'Count'])  # Write header to CS
+            
+            for i in self.bigrams:
+                for j in self.bigrams[i]:
+                    l.append((self.bigrams[i][j]/self.totalcount, i + " " + j))
+            l.sort(reverse=True)
+            top5 = l[:5]
+
+            for count, bigram in top5:
+                csv_writer.writerow([bigram, count])
+                
+    def update_corpus(self,corpus):
+        with open("corpus2.txt","w") as f1:
+            with open(corpus,"r") as f2:
+                sentences=f2.readlines()
+                for i in sentences:
+                    i=i.strip()
+                    f1.write("# "+i+" $\n")
+                    
+    def p_first(self,corpus):
+            with open(corpus) as f:
+                words = f.read().split()
+                d={}
+                total=0
+                for i in range(len(words)):
+                    if (words[i]==self.bol):
+                        if(words[i+1] in d):
+                            d[words[i+1]]+=1
+                        else:
+                            d[words[i+1]]=1
+                        total+=1
+                for i in d:
+                    d[i] = d[i]/total
+                d2={'word':d.keys(),'prob':d.values()}
+                df=pd.DataFrame(d2)
+                df.to_csv("first_prob.csv")
+                
+                
+
 
 b1=BigramLM()
 b1.add_data("corpus.txt")
+# print(b1.bigrams)
 # b1.learn()
 # b1.KneserNey_learn()
-b1.Laplace_learn()
-print(b1.findsentenceprob("i stand here i feel empty a class post count link href http mooshilu"))
+# b1.Laplace_learn()
+# b1.find_top5_bigrams()
+# b1.update_corpus("corpus.txt")
+# b1.p_first("corpus2.txt")
+# print(b1.findsentenceprob("i stand here i feel empty a class post count link href http mooshilu"))
+
+#Create csvs for kneser and laplace and add kneser ney freq calculation
