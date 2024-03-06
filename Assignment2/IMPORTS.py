ae7c6d708e95508b245bcfeb192f0ee206aeacd9
RaghavSakhuja <raghav21274@iiitd.ac.in>
2024-03-06 20:59:56 +0530

    callbacks, handelling imports etc

---
+++ Assignment2/IMPORTS.py
@@ -0,0 +1,172 @@
+from gensim.models import KeyedVectors
+import matplotlib.pyplot as plt
+from keras.preprocessing.text import *
+import json
+import numpy as np
+import pickle
+from keras.callbacks import Callback
+from keras.metrics import F1Score, Precision, Recall
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers import Embedding
+from keras.layers import GRU, SimpleRNN
+from keras.layers import TimeDistributed
+from keras.layers import Dense
+from keras.utils import to_categorical
+from keras.preprocessing.sequence import pad_sequences
+from sklearn.metrics import f1_score
+
+def checking(a):
+    print("Hello World",a)
+
+def new_checking(a):
+    print("Hello World",a)
+
+def create_train_test_val(file):
+    lst_out_text=[]
+    lst_out_labels=[]
+    for i in file.values():
+        lst =[]
+        sentence = i['text']
+        sentence = sentence.lower()
+        labels = i['labels']
+        lst_out_text.append(sentence)
+        lst_out_labels.append(labels)
+    return lst_out_text,lst_out_labels    
+
+def TokenCreator(sentences,val,tokenizer=None):
+    
+    temp_wordlist = None
+    tokenizer2=None
+    if(tokenizer == None):
+        if val==1:
+            tokenizer2 = Tokenizer(oov_token="#UK",filters='')
+        else:
+            tokenizer2=Tokenizer()
+        tokenizer2.fit_on_texts(sentences)
+        
+        temp_wordlist = tokenizer2.texts_to_sequences(sentences)
+        return (temp_wordlist,tokenizer2)
+    
+    else:
+        temp_wordlist = tokenizer.texts_to_sequences(sentences)
+        return temp_wordlist
+    
+def find_vocab(dataset):
+    lst=[]
+    lst.append("#UK")
+    for i in dataset:
+        for j in i:
+            lst.append(j)
+    st = set(lst)
+    return st
+
+'''
+--------------------------------------GRAPHS--------------------------------------
+'''
+
+def draw_loss_graph(model,title,f1_scores_train,f1_scores_val):
+    train_loss = model.history['loss']
+    val_loss = model.history['val_loss']
+    x = [i+1 for i in range(len(train_loss))]
+    train_y = train_loss
+    val_y = val_loss
+    
+    fig, axes = plt.subplots(1,2,figsize = (16,5))
+    # plt.subplots(axes = (2,1))
+    axes[0].plot(x,train_y, color = 'blue')  
+    axes[0].set_title('Training Loss vs Epoch')
+    axes[0].set_xlabel('Epochs')
+    axes[0].set_ylabel('Training Loss')
+    
+    axes[0].plot(x,val_y, color = 'red') 
+    axes[0].set_title('Validation Loss over Epochs')
+    axes[0].set_xlabel('Epoch')
+    axes[0].set_ylabel('Validation Loss')
+
+    axes[1].plot(x,f1_scores_train, color = 'blue')
+    axes[1].set_title('Training F1 Score vs Epoch')
+    axes[1].set_xlabel('Epochs')
+    axes[1].set_ylabel('Training F1 Score')
+
+    axes[1].plot(x,f1_scores_val, color = 'red')
+    axes[1].set_title('Validation F1 Score vs Epoch')
+    axes[1].set_xlabel('Epochs')
+    axes[1].set_ylabel('Validation F1 Score')
+
+    axes[0].legend(['Train','Val'])
+    axes[1].legend(['Train','Val'])
+    
+    plt.suptitle(title)
+
+    
+    plt.show()
+    
+'''
+--------------------------------------MODELS--------------------------------------
+'''
+
+def save_model(model, name, embedding, task):
+    with open(f'Saved_Models/{task}_{name}_{embedding}.pkl', 'wb') as file:
+        pickle.dump(model, file)
+
+def load_model(file_name):
+    with open(f'{file_name}', 'rb') as file:
+        loaded_model = pickle.load(file)
+    
+    return loaded_model
+
+'''
+--------------------------------------CALLBACKS--------------------------------------
+'''
+
+class F1ScoreCallback(Callback):
+
+    def __init__(self, data, name):
+        super(F1ScoreCallback, self).__init__()
+        self.data = data
+        self.name= name
+        self.f1_scores = [] 
+
+    
+
+    def on_epoch_end(self, epoch, logs=None):
+        x_val, y_val = self.data
+        y_pred = self.model.predict(x_val)
+        def get_pred(Y_padded_output):
+                final_output=[]
+                for i in range(Y_padded_output.shape[0]):
+                    output=[]
+                    for j in range(Y_padded_output.shape[1]):
+                        max_value=-1
+                        maxIndex=-1
+                        for k in range(Y_padded_output.shape[2]):
+                            if Y_padded_output[i][j][k]>max_value:
+                                maxIndex=k
+                                max_value=max(max_value,Y_padded_output[i][j][k])
+                        output.append(maxIndex)
+                    final_output.append(output)
+                return final_output
+
+        def get_real(Y_padded_test):
+            final_Y=[]
+            for i in range(Y_padded_test.shape[0]):
+                output=[]
+                for j in range(Y_padded_test.shape[1]):
+                    max_value=-1
+                    maxIndex=-1
+                    for k in range(Y_padded_test.shape[2]):
+                        if Y_padded_test[i][j][k]>max_value:
+                            maxIndex=k
+                            max_value=max(max_value,Y_padded_test[i][j][k])
+                    output.append(maxIndex)
+                final_Y.append(output)
+            return final_Y
+        prediction=get_pred(y_pred)
+        real=get_real(y_val)
+
+        f1 = F1Score(average='macro')
+        f1.update_state(real,prediction)
+        result = f1.result().numpy()
+        print(f'{self.name} F1 Score: {result}')
+        self.f1_scores.append(result)
